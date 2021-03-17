c710fe49184d7dcec5c5bf22e955f3123ba2d777
Juliana Behringer <julianabgr@hotmail.com>
2021-03-17 15:17:17 -0300

    App utilizando Preferencias de Usuário para salvar anotações

---
+++ app/src/main/java/com/example/minhasanotaes/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.minhasanotaes;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AnotacaoPreferencias preferencias;
+    private EditText editAnotacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editAnotacao = findViewById(R.id.editAnotacao);
+
+        preferencias = new AnotacaoPreferencias(getApplicationContext());
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Validar se foi digitado algo
+                String textoRecuperado = editAnotacao.getText().toString();
+                if(textoRecuperado.equals("")) {
+                    Snackbar.make(view, "Preencha a Anotação!", Snackbar.LENGTH_LONG).show();
+                }else {{
+
+                }
+                    preferencias.salvarAnotacao(textoRecuperado);
+                    Snackbar.make(view, "Anotação Salva com Sucesso!", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+        //Recuperar anotação
+        String anotacao = preferencias.recuperarAnotacao();
+        if( !anotacao.equals("")){ //negaçao !
+            editAnotacao.setText(anotacao);
+        }
+    }
+
+}
